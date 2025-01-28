0beff37621a54e4e32b119996c5991d5ef0814e1
Lukas Prediger <lukas.prediger@csc.fi>
2025-01-28 10:14:31 +0200

    Updating bonus_material/exercise_container_recipes

--- bonus_material/exercise_container_recipes/build_pytorch_transformers.sh
+++ bonus_material/exercise_container_recipes/build_pytorch_transformers.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 module purge
-#module load cotainr # as of 2024-11-14 the default version of cotainr has a breaking bug
-module load cotainr/2023.11.0-20240909
+module load CrayEnv cotainr
 
 cotainr build pytorch_transformers.sif --system lumi-g --conda-env=./pytorch_transformers.yml
