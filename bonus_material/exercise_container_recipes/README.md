f25016c4dd74ae1f7f423e09e6942626a2864492
Joern Dietze <jdi010@uit.no>
2025-02-03 11:01:32 +0200

    Remove bonus material

--- bonus_material/exercise_container_recipes/README.md
+++
@@ -1,3 +0,0 @@
-# Exercise Container Recipes
-
-This folder contains the cotainr recipes for building the container used in Exercise 3 and 8.
--- bonus_material/exercise_container_recipes/build_pytorch_transformers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-module purge
-#module load cotainr # as of 2024-11-14 the default version of cotainr has a breaking bug
-module load cotainr/2023.11.0-20240909
-
-cotainr build pytorch_transformers.sif --system lumi-g --conda-env=./pytorch_transformers.yml
